73ba90cfd8734be2144459a12af845a12e2b03e8
Yakov <yashahus@gmail.com>
2022-05-06 17:00:20 +0700

    первое практическое задание

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 "use strict"
-// console.log(4 +'5');
-const isChecked = true,
-      isClose = true;
-console.log(isChecked || !isClose);
+// // console.log(4 +'5');
+// const isChecked = true,
+//       isClose = true;
+// console.log(isChecked || !isClose);
+
+const numberFiles = +prompt('Сколько фильмов вы уже посмотрели', '');
+
+
+ const personalMovieDB= {
+     count: numberFiles,
+     movies:{},
+     actors:{},
+     geners:[],
+     privat: false4
+ };
+
+
+     const a = 'kjufy',
+           b = prompt('На сколько вы его оцениваете','');
+         
+   personalMovieDB.movies[a]= b;
+   console.log(personalMovieDB);
+
+
